ebee2f9aab143036d3c7e4f872c8413b1a41af07
fastfoxes <38584458+fastfoxes@users.noreply.github.com>
2019-11-22 09:25:35 +0800

    debug

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ apt-get install gimp gimpshop krita # 绘图
 apt-get install blender # 3D
 apt-get install typora vim # markdown
 apt-get install Koffice libre office # office
-apt-get install c c++ python # 编程
-apt-get install clang GCC python3 ## 编译器
+apt-get install clang GCC python3 python2 python3-dev python2-dev # 编译器
+apt-get install qt 
 apt-get install cmake make ## 自动构建
 apt-get install grep ## 文本查找
 apt-get install virtual box # 虚拟机
