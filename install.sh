b700cd210ee58d748eeeffe31e22646d8b79315a
fastfoxes <38584458+fastfoxes@users.noreply.github.com>
2019-11-22 09:11:23 +0800

    turn into bash

---
+++ install.sh
@@ -0,0 +1,13 @@
+# 系统 Ubuntu Centos Deepin cygwin
+apt-get install gimp gimpshop krita # 绘图
+apt-get install blender # 3D
+apt-get install typora vim # markdown
+apt-get install Koffice libre office # office
+apt-get install c c++ python # 编程
+apt-get install clang GCC python3 ## 编译器
+apt-get install cmake make ## 自动构建
+apt-get install grep ## 文本查找
+apt-get install virtual box # 虚拟机
+apt-get install firefox # 浏览器
+apt-get install wget # 网页下载
+apt-get install Redshift # 护眼
